379d6c522fbfddb046742682a420f11f3d0045de
@theStupidProwler <alexandre.ouillon@protonmail.com>
2017-06-12 13:47:13 +0200

    oui

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
-def readNames():
-    
+def readAssets(typeOfAssets):
+    fileName = "assets/" + typeOfAssets + ".txt"
+    asset = open(fileName, 'r')
+    list[0] = typeOfAssets
+    inc = 0
+    while(lineContent)
+    {
+        inc = inc + 1
+        line[inc] = lineContent
+        lineContent = asset.read()
+    }
+readAssets()
