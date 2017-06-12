1e331e6fe48e7ac0e61b348631a3432a987bbcca
@theStupidProwler <alexandre.ouillon@protonmail.com>
2017-06-12 14:47:19 +0200

    adding (shitty)probability to algorithm

--- main.py
+++ main.py
@@ -9,4 +9,5 @@ gender = "M"
 inc = 0
 listNames = readAssets("familyNames")
 listFirstNames = readAssets("firstNames"+ gender)
+print(listFirstNames[randrange(0,randrange(0,len(listFirstNames)))])
 print(listNames[randrange(0,len(listNames))])
